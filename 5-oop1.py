cd7c380ce015b2e2c855b287d44cd0f25cbad5aa
Yosua Y Ariyanto <yosua96yudi@gmail.com>
2020-06-20 00:31:52 +0700

    Add files via upload

---
+++ 5-oop1.py
@@ -0,0 +1,7 @@
+#ubah task 1 dengan menggunakan metode oop dan buat fungsi yang dibutuhkan
+class world:
+    def bilangHaloWorld(self):
+        print('Halo World')
+
+org = world()
+org.bilangHaloWorld()
