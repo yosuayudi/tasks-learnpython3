7e0d66bc905834395ae7773fd5f52c5dc4855d7b
Yosua Y Ariyanto <yosua96yudi@gmail.com>
2020-06-19 23:44:02 +0700

    Update 1-helloworld.py

--- 1-helloworld.py
+++ 1-helloworld.py
@@ -1,2 +1,12 @@
 #TUGAS 1
 #BUAT OUTPUT YANG MENAMPILKAN TULISAN HELLO WORLD DIBAWAH
+print('Program menghitung volume')
+p = int(input('masukan panjang : '))
+l = int(input('masukan lebar : '))
+t = int(input('masukan tinggi : '))
+
+def volume(p,l,t):
+    V = p * l * t
+    return V
+
+print('Jadi balok dengan ukuran panjang:{}, lebar:{}, tinggi:{} \nMempunyai volume:{}'.format(p,l,t, volume(p,l,t)))
