3dd133843807e99dab1cb77cbf06735fd251a1aa
Yosua Y Ariyanto <yosua96yudi@gmail.com>
2020-06-20 00:02:20 +0700

    Update 3-operator.py

--- 3-operator.py
+++ 3-operator.py
@@ -2,3 +2,13 @@
 #BUAT PERHITUNGAN RUMUS VOLUME PERSEGI PANJANG MENGGUNAKAN PYTHON, MASING-MASING NILAI VARIABEL DIINPUTKAN COMMAND KEMUDIAN KETIKA KODE DIEKSEKUSI MAKA AKAN MENAMPILKAN HASIL DARI PERHITUNGAN TADI
 # RUMUS VOLUME = PANJANG X LEBAR X TINGGI
 # BUAT VARIABEL SESUAI YANG DIBUTUHKAN DALAM VARIABEL TERSEBUT
+print('Program menghitung volume')
+p = int(input('masukan panjang : '))
+l = int(input('masukan lebar : '))
+t = int(input('masukan tinggi : '))
+
+def volume(p,l,t):
+    V = p * l * t
+    return V
+
+print('Jadi balok dengan ukuran panjang:{}, lebar:{}, tinggi:{} \nMempunyai volume:{}'.format(p,l,t, volume(p,l,t)))
